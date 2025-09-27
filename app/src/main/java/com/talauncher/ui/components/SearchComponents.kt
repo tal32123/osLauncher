4defca814cfaf9e49e4fdcf447dcb8d437a4c143
Tal Charnes <tal32123@gmail.com>
2025-09-27 08:47:21 +0300

    Add Compose testTag imports to components and settings

--- app/src/main/java/com/talauncher/ui/components/SearchComponents.kt
+++ app/src/main/java/com/talauncher/ui/components/SearchComponents.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.talauncher.R
